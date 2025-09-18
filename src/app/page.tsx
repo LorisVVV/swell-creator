0e346bef0dec5e5203d2165883becee126cef751
Lolorisotto <lorisvullion@gmail.com>
2025-09-17 22:06:17 -0400

    Working on the GUI to add and remove waves

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,20 @@
 "use client"
 
-import styles from "./page.module.css";
-import { Canvas, useThree } from "@react-three/fiber";
-import Wave from "@/component/Wave";
-import { OrbitControls } from "@react-three/drei";
-import FPSStats from "react-fps-stats";
+// import styles from "./page.module.css";
+// import { Canvas, useThree } from "@react-three/fiber";
+// import Wave from "@/component/Wave";
+// import { OrbitControls } from "@react-three/drei";
+// import FPSStats from "react-fps-stats";
+// import { readFileSync } from "fs";
+import Scene from "@/component/Scene";
+// import { readFileSync } from "fs";
 
 export default function Home() {
 
-  return (
-        <div className={styles.scene}>
-          <FPSStats />
-          <Canvas
-              shadows
-              className={styles.canvas}
-              camera={{
-                position: [-6, 7, 7],
-              }}
-                   
-            >      
-              <Wave debug/>
-              <ambientLight intensity={2} />
-              <OrbitControls/>   
 
-            </Canvas>
+  return (
+        <div>
+          <Scene readFile={() => {}}/>
         </div>
   );
 }
